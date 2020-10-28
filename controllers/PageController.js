e93eddd26d9c33c7e63bbf8b9999ed95beecdade
Jeanjon Valentin <valentin.jeanon@unilim.fr>
2020-10-28 21:44:48 +0000

    Add all page and imlementation of contact

--- controllers/PageController.js
+++ controllers/PageController.js
@@ -2,6 +2,16 @@ let model = require('../models/model.js');
 let async = require('async');
 
 module.exports.Home =  function(request, response) {
-    response.title = "Home";
+    response.title = "Home - Inside Out by design";
     response.render('home', response);
+};
+
+module.exports.About =  function(request, response) {
+    response.title = "About us - Inside Out by design";
+    response.render('about-us', response);
+};
+
+module.exports.Contact =  function(request, response) {
+    response.title = "Contact - Inside Out by design";
+    response.render('contact', response);
 };
--- router/router.js
+++ router/router.js
@@ -2,4 +2,9 @@ let PageController = require('../controllers/PageController');
 
 module.exports = function(app) {
     app.get('/', PageController.Home);
+    app.get('/home', PageController.Home);
+
+    app.get('/about-us', PageController.About);
+
+    app.get('/contact', PageController.Contact);
 };
