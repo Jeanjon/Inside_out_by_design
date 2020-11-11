b4f9f645d4dea87d1373daf14d657cd8327eebef
Jeanjon Valentin <valentin.jeanon@unilim.fr>
2020-11-11 15:37:29 +0000

    change path

--- router/router.js
+++ router/router.js
@@ -3,13 +3,13 @@ let PageController = require('../controllers/PageController');
 module.exports = function(app) {
     app.get('/', PageController.Home);
     app.get('/home', PageController.Home);
-    app.post('/home',PageController.Login);
 
     app.get('/about-us', PageController.About);
 
     app.get('/contact', PageController.Contact);
 
     app.get('/sign-in-up', PageController.Sign);
+    app.post('/sign-in',PageController.Login);
     app.get('/sign-up', PageController.SignUp);
     app.post('/sign-up', PageController.CreateAccount);
 
